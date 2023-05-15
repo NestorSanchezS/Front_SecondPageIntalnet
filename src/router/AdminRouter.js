35d485562bb11329ee7e8914fd05a12d0aa043be
NestorSanchez <nesaenz.69@gmail.com>
2023-05-15 11:35:51 -0500

    add two router

--- src/router/AdminRouter.js
+++ src/router/AdminRouter.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Auth } from "../pages/admin";
+import { Auth, Users } from "../pages/admin";
 import { AdminLayout } from "../layouts/AdminLayout";
 
-const user = null;
+const user = "Mi compa";
 export function AdminRouter() {
   const loadLayout = (Layout, Page) => {
     return (
@@ -14,7 +14,13 @@ export function AdminRouter() {
   };
   return (
     <Routes>
-      <Route path="/admin/*" element={loadLayout(AdminLayout, Auth)} />
+      {!user ? (
+        <Route path="/admin/*" element={loadLayout(AdminLayout, Auth)} />
+      ) : (
+        <>
+          <Route path="/admin/users" element={loadLayout(AdminLayout, Users)} />
+        </>
+      )}
     </Routes>
   );
 }
